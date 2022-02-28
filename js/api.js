812afb386e205a2e63d260bc901a3b33df828ad9
AIA <adashik585@gmail.com>
2022-03-01 00:28:36 +0600

    search phone field added

--- js/api.js
+++ js/api.js
@@ -1,1 +1,23 @@
-console.log('api conected')
+/* 
+Phone Search
+URL Format: https://openapi.programming-hero.com/api/phones?search=${searchText}
+
+Example: https://openapi.programming-hero.com/api/phones?search=iphone
+
+Phone detail url:
+URL Format: https://openapi.programming-hero.com/api/phone/${id}
+
+Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
+*/
+const searchPhone = () => {
+    const phoneField = document.getElementById('phone-input');
+    const phoneValue = phoneField.value;
+    console.log(phoneValue);
+    phoneField.value = '';
+
+    // dynamic phone url
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
