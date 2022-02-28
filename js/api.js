ff07c46171c632062f0bd93baf280936c4b715db
AIA <adashik585@gmail.com>
2022-03-01 03:13:29 +0600

    search phone area

--- js/api.js
+++ js/api.js
@@ -9,15 +9,35 @@ URL Format: https://openapi.programming-hero.com/api/phone/${id}
 
 Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
 */
+// search phone area
 const searchPhone = () => {
     const phoneField = document.getElementById('phone-input');
     const phoneValue = phoneField.value;
-    console.log(phoneValue);
     phoneField.value = '';
 
     // dynamic phone url
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${phoneValue}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => serachDisplayPhone(data.data));
+}
+
+// display phone
+const serachDisplayPhone = data => {
+    const displayPhone = document.getElementById('display-phone');
+    data.forEach(data => {
+        console.log(data);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                    to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `
+    });
 }
